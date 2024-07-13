d7c5e83c368814adaad1e51e8588294bd14c8c1b
riskiputraalamzah <aryainggil3@gmail.com>
2024-07-13 20:28:05 +0700

    try add ar js

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar bg-dark">
     <div class="container-fluid justify-content-start">
       <a @click="$router.back()" class="navbar-brand">
-        <i class="bi bi-arrow-left-short fs-1 fw-bold text-light"></i>
+        <i class="bi bi-arrow-left-short display-3 fw-bold text-light"></i>
       </a>
       <div class="font-roboto fs-3">
         <slot></slot>
