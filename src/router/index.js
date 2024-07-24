40570eec0824cbc9e0267a58fd46b4c3f43df6f5
riskiputraalamzah <aryainggil3@gmail.com>
2024-07-24 16:15:02 +0700

    add scroll to top

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,10 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
+  scrollBehavior(to, from, savedPosition) {
+    // always scroll to top
+    return { top: 0 };
+  },
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
